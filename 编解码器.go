87a4c419bc1dc7a830e49a96c4d5e6e5700955d6
Tank-cv <tanktr72@gmail.com>
2025-12-31 23:09:08 +0800

    Add files via upload

--- 编解码器.go
+++ 编解码器.go
@@ -1,38 +1,39 @@
 package main
 
 import (
-	 "encoding/base64"
-	 "encoding/hex"
+	"encoding/base64"
+	"encoding/hex"
 	f "fmt"
-	 "net/url"
-	 "strings"
+	"net/url"
 )
+
+// 先将要调配的函数写出来
 
 func menu() int {
 	var ch int
-	f.Println("1编码，2解码，eixt退出")
+	f.Println("1编码，2解码，0退出")
 	f.Scan(&ch)
 	return ch
 
 }
 func encmenu() int {
 	var ch int
-	f.Println("编码：1,base64 2,url 3,hex eixt退出")
+	f.Println("编码：1,base64 2,url 3,hex 0退出")
 	f.Scan(&ch)
 	return ch
 }
 func decmenu() int {
 	var ch int
-	f.Println("解码：1,base64 2,url 3,hex eixt退出")
+	f.Println("解码：1,base64 2,url 3,hex 0退出")
 	f.Scan(&ch)
 	return ch
 }
 func base64enc(str string) string {
-	var []byte
-	return base64.StdEncoding.EncodeToString([]byte(str))
+	var b []byte = []byte(str)
+	return base64.StdEncoding.EncodeToString(b)
 }
-func base64dec(str string) (string,error) {
-	var d,err =base64.StdEncoding.DecodeString(str)
+func base64dec(str string) (string, error) {
+	var d, err = base64.StdEncoding.DecodeString(str)
 	return string(d), err
 }
 func urleco(str string) string {
@@ -40,14 +41,74 @@ func urleco(str string) string {
 	return url.QueryEscape(str)
 
 }
-func urldeco(str string)(string,error) {
+func urldeco(str string) (string, error) {
 	var str string
 	return url.QueryUnescape(str)
 }
 func hexenc(str string) string {
-	var []byte
-	return hex.EncodeToString([]byte(str))
+	var b []byte = []byte(str)
+	return hex.EncodeToString(b)
 }
-func hexdec(str string) (string,error) {
-	var	d,err =hex.DecodeString(str)
+func hexdec(str string) (string, error) {
+	var d, err = hex.DecodeString(str)
+	return string(d), err
+}
+
+// 进入主程序，开始写逻辑
+func main() {
+	for {
+		choice := menu()
+		switch choice {
+		case 1:
+			for {
+				ech := encmenu()
+				if ech == 0 {
+					f.Println("返回主菜单")
+					break
+				}
+				var input string
+				f.Println("请输入")
+				f.Scanln(&input)
+				var ans string
+				switch ech {
+				case 1:
+					ans = base64enc(input)
+				case 2:
+					ans = hexenc(input)
+				case 3:
+					ans = urleco(input)
+				}
+				f.Printf("%s\n", ans)
+
+			}
+		case 2:
+			for {
+				dec := decmenu()
+				if dec == 0 {
+					f.Println("返回主菜单")
+					break
+				}
+				var input string
+				f.Println("请输入")
+				f.Scanln(&input)
+				ans, err := "", error(nil)
+				switch dec {
+				case 1:
+					ans, err = base64dec(input)
+				case 2:
+					ans, err = hexdec(input)
+				case 3:
+					ans, err = urldeco(input)
+				}
+				if err != nil {
+					f.Println("解码错误", err)
+				} else {
+					f.Println("解码成功", ans)
+				}
+			}
+		default:
+			f.Println("程序退出")
+			return
+		}
+	}
 }
