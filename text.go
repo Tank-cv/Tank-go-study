81c7b28df6c9fa8fc7e51d8f287942e0816b60b8
Tank-cv <tanktr72@gmail.com>
2026-01-08 00:32:24 +0800

    Add files via upload
    修改了一下逻辑

--- text.go
+++ text.go
@@ -5,8 +5,8 @@ import (
 	"sync"
 )
 
-var cashier1chan = make(chan int, 10000)
-var cashier2chan = make(chan int, 10000)
+var cashier1chan = make(chan string, 10000)
+var cashier2chan = make(chan string, 10000)
 var wait sync.WaitGroup
 
 var productMap = map[string]int{
@@ -29,13 +29,21 @@ type producemap struct {
 
 func cashier1() {
 	var total int
+	println("请输入商品名称，输入exit退出")
+	for{
+		var input string
+		fmt.Scanln(&input)
+		cashier1chan<-input
+		if input=="exit"{
+			break
+		}
+	}
 	for id := range cashier1chan {
 		var productName string
-		fmt.Printf("顾客%d 到达收银台1，请输入商品名称: ", id)
+		fmt.Printf("收银台1，商品名称: %s", id)
 		fmt.Scanln(&productName)
 		price, ok := productMap[productName]
 		if !ok {
-			fmt.Printf("收银台1：未找到商品 %s，价格视为0\n", productName)
 			continue
 		}
 		total += price
@@ -46,13 +54,21 @@ func cashier1() {
 }
 func cashier2() {
 	var total int
-	for id := range cashier2chan {
+	println("请输入商品名称，输入exit退出")
+	for{
+		var input string
+		fmt.Scanln(&input)
+		cashier1chan<-input
+		if input=="exit"{
+			break
+		}
+	}
+	for id := range cashier1chan {
 		var productName string
-		fmt.Printf("顾客%d 到达收银台2，请输入商品名称: ", id)
+		fmt.Printf("收银台2，商品名称: %s", id)
 		fmt.Scanln(&productName)
 		price, ok := productMap[productName]
 		if !ok {
-			fmt.Printf("收银台2：未找到商品 %s，价格视为0\n", productName)
 			continue
 		}
 		total += price
@@ -60,14 +76,6 @@ func cashier2() {
 	}
 	fmt.Printf("收银台2总计: %d\n", total)
 	wait.Done()
-}
-func produce(ch chan<- int) {
-	// 模拟一定数量的顾客到达，只将顾客发送到目标收银台的 channel
-	for i := 1; i <= 5; i++ {
-		fmt.Printf("客人%d 到达队列\n", i)
-		ch <- i
-	}
-	close(ch)
 }
 func main() {
 	println("====收银系统====")
@@ -79,11 +87,11 @@ func main() {
 	case 1:
 		wait.Add(1)
 		go cashier1()
-		go produce(cashier1chan)
+		
 	case 2:
 		wait.Add(1)
 		go cashier2()
-		go produce(cashier2chan)
+		
 	default:
 		fmt.Println("无效选择，退出")
 		return
