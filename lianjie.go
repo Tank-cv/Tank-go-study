6c9d24a888b317fe19556b4c74702035bd691eae
Tank-cv <tanktr72@gmail.com>
2026-01-15 00:18:28 +0800

    Add files via upload

--- lianjie.go
+++ lianjie.go
@@ -5,9 +5,18 @@ import (
 	"net"
 )
 
+// 垃圾代码生产商  垃圾+1  ’（-_-）‘
 func main() {
+	fmt.Println("||========极简抓包========||")
+	var host string
+	fmt.Println("请输入网址")
+	fmt.Scanln(&host)
 
-	target := "node4.anna.nssctf.cn:25939"
+	fmt.Println("请输入端口")
+	var port string
+	fmt.Scanln(&port)
+
+	target := host + ":" + port
 
 	conn, err := net.Dial("tcp", target)
 	if err != nil {
@@ -16,7 +25,7 @@ func main() {
 	}
 	defer conn.Close()
 
-	req := "GET /index.php HTTP/1.1\r\nHost: node4.anna.nssctf.cn\r\nConnection: close\r\n\r\n"
+	req := "GET / HTTP/1.1\r\nHost: " + host + "\r\n"
 	_, err = conn.Write([]byte(req))
 	if err != nil {
 		fmt.Printf("发送请求失败：%v\n", err)
@@ -24,18 +33,52 @@ func main() {
 	}
 
 	var Data []byte
-	buf := make([]byte, 10000)
+	buf := make([]byte, 100000)
 	for {
 		n, err := conn.Read(buf)
 		if n > 0 {
 			Data = append(Data, buf[:n]...)
 		}
 		if err != nil {
+			fmt.Printf("读取数据失败：%v\n", err)
 			break
 		}
 	}
 
-	fmt.Println("\n=== 从目标读取的数据包内容 ===")
-	fmt.Println(string(Data))
+	k := 0
+	var counnt int = 0
+	for range Data {
 
+		if k+3 >= len(Data) {
+			break
+		}
+
+		if Data[k] == 'f' && Data[k+1] == 'l' && Data[k+2] == 'a' && Data[k+3] == 'g' {
+			fmt.Println("\n== 找到flag ==")
+
+			if k+4 < len(Data) && Data[k+4] == '{' {
+				j := k + 5
+
+				for j < len(Data) && Data[j] != '}' {
+					j++
+				}
+
+				if j < len(Data) {
+					fmt.Println("===========flag===========")
+					fmt.Println("flag{", string(Data[k+5:j]), "}")
+					fmt.Println("=========================")
+					counnt = 1
+					break
+				}
+			}
+		}
+		k += 1
+	}
+
+	if counnt == 0 {
+		fmt.Println("\n== 未找到flag ==")
+	}
+
+	fmt.Println("\n== 从目标读取的数据包内容 ==")
+	fmt.Println(string(Data))
 }
